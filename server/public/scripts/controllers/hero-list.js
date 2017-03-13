e6f68827934b93b488563a62d72fcf593a36860b
Celina Peterson <celinap@gmail.com>
2017-03-13 13:35:53 -0500

    building getHeroes function and placing that data in hero-list.html

--- server/public/scripts/controllers/hero-list.js
+++ server/public/scripts/controllers/hero-list.js
@@ -1,3 +1,17 @@
-app.controller('HeroListController', function(){
-    console.log('Hero List Controller loaded');
-});
+app.controller('HeroListController', ['$http', function($http) {
+  console.log('Hero List Controller loaded');
+
+  var self = this;
+  self.testMessage = "This is my test message";
+  self.heroes = [];
+
+  getHeroes();
+
+  // get all employee data
+  function getHeroes() {
+    $http.get('/heroes').then(function(response) {
+      self.heroes = response.data;
+    });
+  }
+
+}]);
